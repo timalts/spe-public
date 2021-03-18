bd860d633e5ee423cf5ac7d7e62cce74a4a50bf7
Tim N <timothe.noel67@gmail.com>
2021-03-18 17:47:05 +0000

    Login system (week 5 back-end)

---
+++ back-end/backend-week5/backend-week5/DTO/UpdateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_week5.DTO
+{
+    public class UpdateModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
---
+++ back-end/backend-week5/backend-week5/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using backend_week5.DTO;
+using backend_week5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_week5.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<RegisterModel, User>();
+            CreateMap<UpdateModel, User>();
+        }
+    }
+}
